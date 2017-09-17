dc91f71ddef67d400f8a0fc5645280b599abfbbf
Marcelo Buratti de Freitas <marcelofreitas21@hotmail.com>
2017-09-17 18:46:27 -0300

    Brincando com l5-repository

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MARCELO
+ * Date: 17/09/2017
+ * Time: 18:41
+ */
+
+namespace CodeFlix\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
