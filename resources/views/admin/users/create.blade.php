774df86bf20166a3167b12346ee1a44799a041b6
Marcelo Buratti de Freitas <marcelofreitas21@hotmail.com>
2017-09-16 22:19:31 -0300

    Laravel Mix - Reloading

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>Novo Usuário</h3>
+            <h3>Novo Usuário: </h3>
             <?php  $icon = Icon::create('floppy-disk');?>
            {!! form($form->add('Salve','submit',[
                 'attr' => ['class' => 'btn btn-primary btn-block'],
-                'label' => $icon
+                'label' => $icon. " Salvar"
             ])) !!}
         </div>
 
